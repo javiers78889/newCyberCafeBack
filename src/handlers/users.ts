c5813f41931e5c0074323dc0a3c2edd732ee2cc8
javiers78889 <157335485+javiers78889@users.noreply.github.com>
2024-09-14 00:35:43 -0500

    Update users.ts

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -35,3 +35,30 @@ export const selectUsers = async (req: Request, res: Response) => {
         res.status(500).json({ message: 'Error al crear el usuario' }); // Devuelve un estado 500 en caso de error
     }
 };
+
+export const UpdateUsers = async (req: Request, res: Response) => {
+    const { usuario, contraseña } = req.body;
+
+    try {
+        // Verifica si el usuario existe
+        const user = await Users.findOne({ where: { usuario } });
+
+        if (!user) {
+            return res.status(404).json({ message: 'Usuario no encontrado' });
+        }
+
+        // Actualiza la contraseña del usuario
+        const [updateCount] = await Users.update(
+            { contraseña }, // Nueva contraseña
+            { where: { usuario } } // Usuario a actualizar
+        );
+
+        if (updateCount > 0) {
+            return res.status(200).json({ message: 'Usuario actualizado con éxito' });
+        } else {
+            return res.status(404).json({ message: 'Usuario no encontrado' });
+        }
+    } catch (error) {
+        return res.status(500).json({ message: 'Error al actualizar el usuario', error });
+    }
+};
