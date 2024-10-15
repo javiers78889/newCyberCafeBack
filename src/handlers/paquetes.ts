0085066509680346e4f54fc4744f476eff5a1ad1
javiers78889 <javiers78889@gmail.com>
2024-10-14 22:41:18 -0500

    codigo optimizado

--- src/handlers/paquetes.ts
+++ src/handlers/paquetes.ts
@@ -1,91 +1,69 @@
 import { Request, Response } from 'express';
 import Paquetes from '../models/paquetes.model';
 import { validationResult, check } from 'express-validator';
-import Twilio from 'twilio';
-import dotenv from 'dotenv';
-dotenv.config();
-
-const accountSidd = process.env.ACCOUNT_SSID;
-const authTokenn = process.env.AUTH_TOKEN
-
-// Configuración de Twilio
-const accountSid = accountSidd;
-const authToken = authTokenn;
-const client = Twilio(accountSid, authToken);
+import { Sender } from '../middleware/sender';
 
 
 export const createPaquetes = async (req: Request, res: Response) => {
-    const paquetes = await Paquetes.create(req.body)
-    await check('usuario').notEmpty().withMessage('El nombre de Usuario esta Vacio').run(req)
-    await check('tracking').notEmpty().withMessage('El tracking esta Vacio').run(req)
-    await check('peso').isNumeric().withMessage('El peso debe ser numerico').notEmpty().withMessage('El nombre de Usuario esta Vacio').run(req)
-    await check('precio').isNumeric().withMessage('El precio debe ser numerico').notEmpty().withMessage('El nombre de Usuario esta Vacio').run(req)
-    await check('tarifa').isNumeric().withMessage('El tarifa debe ser numerico').notEmpty().withMessage('El nombre de Usuario esta Vacio').run(req)
-    const{nombre,telefono,tracking,precio}=req.body
-    try {
-        // Enviar mensaje a través de Twilio
-        const message = await client.messages.create({
-          body: `¡Hola ${nombre}!\n\nSomos CyberCafe Chame ☕,\n\nLe informamos que ha recibido un nuevo paquete📦: \n\n\t📍ID Tracking: ${tracking} \n\t💵Total: $${precio} \n\nPuede verificar su factura a través de nuestra plataforma.`,
-          from: 'whatsapp:+50765281534', // Número de WhatsApp del Sandbox de Twilio
-          to: `whatsapp:${'+507' + telefono}`
-        });
+  await check('usuario').notEmpty().withMessage('El nombre de Usuario esta Vacio').run(req)
+  await check('tracking').notEmpty().withMessage('El tracking esta Vacio').run(req)
+  await check('peso').isNumeric().withMessage('El peso debe ser numerico').notEmpty().withMessage('El nombre de Usuario esta Vacio').run(req)
+  await check('precio').isNumeric().withMessage('El precio debe ser numerico').notEmpty().withMessage('El nombre de Usuario esta Vacio').run(req)
+  await check('tarifa').isNumeric().withMessage('El tarifa debe ser numerico').notEmpty().withMessage('El nombre de Usuario esta Vacio').run(req)
   
-        console.log('Mensaje enviado:', message.sid);
-        res.status(201).send('Paquete creado y mensaje enviado exitosamente');
-      } catch (error) {
-        console.error('Error enviando mensaje:', error);
-        res.status(500).send('Paquete creado pero no se pudo enviar el mensaje');
-      }
-
+  const paquetes = await Paquetes.create(req.body)
+  Sender(req.body)
 
 }
-export const selectPaquetes = async (req: Request, res: Response) => {
-    try {
-        const paquetes = await Paquetes.findAll({
-            order: [
-                ['id', 'DESC']
-            ]
-        })
 
-        res.status(201).json({ data: paquetes });
-    } catch (error) {
-        console.error('Error al crear el usuario:', error);
-        res.status(500).json({ message: 'Error al crear el usuario' }); // Devuelve un estado 500 en caso de error
-    }
+
+export const selectPaquetes = async (req: Request, res: Response) => {
+  try {
+    const paquetes = await Paquetes.findAll({
+      order: [
+        ['id', 'DESC']
+      ]
+    })
+
+    res.status(201).json({ data: paquetes });
+  } catch (error) {
+    console.error('Error al crear el usuario:', error);
+    res.status(500).json({ message: 'Error al crear el usuario' }); // Devuelve un estado 500 en caso de error
+  }
 
 }
 export const updatePaquetes = async (req: Request, res: Response) => {
 
-    const { id } = req.params
-    const product = await Paquetes.findByPk(id)
+  const { id } = req.params
+  const product = await Paquetes.findByPk(id)
 
-    console.log(req.body)
-    await product.update(req.body)
-    await product.save()
+  console.log(req.body)
+  await product.update(req.body)
+  await product.save()
     .then(() => {
-        res.status(201).json({ message: 'Producto actualizado Correctamente' });
+      res.status(201).json({ message: 'Producto actualizado Correctamente' });
     })
     .catch((error) => {
-        res.status(500).json({ message: 'Error al Actualizar el producto', error });
+      res.status(500).json({ message: 'Error al Actualizar el producto', error });
     });
 }
 
 export const deletePaquetes = async (req: Request, res: Response) => {
-    try {
-      const { id } = req.params;
-      const paquete = await Paquetes.findByPk(id);
-  
-      if (!paquete) {
-        return res.status(404).json({ message: 'Paquete no encontrado' });
-      }
-      
-      paquete.destroy()
-  
-  
-      res.status(200).json({ message: 'Paquete eliminado' });
-    } catch (error) {
-      res.status(500).json({ message: 'Error al eliminar el paquete', error });
+  try {
+    const { id } = req.params;
+    const paquete = await Paquetes.findByPk(id);
+
+    if (!paquete) {
+      return res.status(404).json({ message: 'Paquete no encontrado' });
     }
-  };
-  
+
+    paquete.destroy()
+
+
+    res.status(200).json({ message: 'Paquete eliminado' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error al eliminar el paquete', error });
+  }
+};
+
 
---
+++ src/middleware/sender.ts
@@ -0,0 +1,31 @@
+import Twilio from 'twilio';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const accountSidd = process.env.ACCOUNT_SSID;
+const authTokenn = process.env.AUTH_TOKEN
+
+// Configuración de Twilio
+const accountSid = accountSidd;
+const authToken = authTokenn;
+const client = Twilio(accountSid, authToken);
+
+export const Sender = async ({ nombre, telefono, tracking, precio }) => {
+
+    try {
+        
+        const message = await client.messages.create({
+            body: `¡Hola ${nombre}!\n\nSomos CyberCafe Chame ☕,\n\nLe informamos que ha recibido un nuevo paquete📦: \n\n\t📍ID Tracking: ${tracking} \n\t💵Total: $${precio} \n\nPuede verificar su factura a través de nuestra plataforma.`,
+            from: 'whatsapp:+50765281534', // Número de WhatsApp del Sandbox de Twilio
+            to: `whatsapp:${'+507' + telefono}`
+        });
+        if(message){
+            
+            return  console.log('Mensaje enviado:', telefono + message.sid);
+        }
+    } catch (error) {
+        
+        return console.error('Error')
+    }
+
+}
