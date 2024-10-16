0d7bc635bede958c6973b79f745eabd68238379e
javiers78889 <157335485+javiers78889@users.noreply.github.com>
2024-10-15 21:57:57 -0500

    Update sendMessage.ts

--- src/handlers/sendMessage.ts
+++ src/handlers/sendMessage.ts
@@ -1,6 +1,11 @@
 import { Sender } from "../middleware/sender"
 
-export const sendMessage = async (req,res) => {
-    Sender(req.body)
-    res.status(201).json('Mensaje Reenviado')
-}
+export const sendMessage = async (req, res) => {
+    try {
+        await Sender(req.body); // Usa await si Sender es asincrónica
+        res.status(201).json('Mensaje Reenviado');
+    } catch (error) {
+        res.status(500).json({ error: 'Error al enviar el mensaje' });
+    }
+};
+
